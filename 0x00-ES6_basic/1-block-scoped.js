9ea664c6cef94228f8e029d4fbd619437775086e
Emmyy882 <emmaagbaje3@gmail.com>
2024-03-05 23:10:39 +0100

    update

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true; // eslint-disable-next-line no-unused-vars
-    const task2 = false; // eslint-disable-next-line no-unused-vars
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
 
   return [task, task2];
