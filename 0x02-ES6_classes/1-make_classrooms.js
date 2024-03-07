547c89556557f981a7d9955c926c5ad5f455beac
Emmyy882 <emmaagbaje3@gmail.com>
2024-03-07 16:38:27 +0100

    fixed bug

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -4,6 +4,7 @@ export default function initializeRooms() {
   const objArray = [19, 20, 34];
   const newArray = [];
 
+  // eslint-disable-next-line guard-for-in
   for (const i in objArray) {
     const obj = new ClassRoom(objArray[i]);
     newArray.push(obj);
